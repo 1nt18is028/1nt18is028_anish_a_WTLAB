30b6f453d58cf38894f72bbc77c6a0484eaa78cf
1nt18is028 <83984563+1nt18is028@users.noreply.github.com>
2021-07-18 21:33:37 +0530

    clock

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-var a = Math.floor(Math.random())* Math.floor(101);
-
-function check(){
-    var num=document.getElementById("num").value;
-    if(num==a){
-        alert("The number is correct");
-    }else if(num+10 < a){
-        alert("")
-    }else if
+function setDate(){
+    const date=new Date();
+    let seconds=date.getSeconds();
+    let minutes=date.getMinutes();
+    let hours=date.getHours();
+    const sh=document.getElementsByClassName("seconds");
+    const mh=document.getElementsByClassName("minutes");
+    const hh=document.getElementsByClassName("hours");
+    console.log(seconds);
+    if(seconds==0)
+     console.log(minutes);
+    if(minutes==0)
+      console.log(hours);
+    seconds=((seconds/60)*360)+90;
+    minutes=((minutes/60)*360)+90;
+    hours=((hours/12)*360)+90;
+    sh[0].style.transform = `rotate(${seconds}deg)`;
+    mh[0].style.transform = `rotate(${minutes}deg)`;
+    hh[0].style.transform = `rotate(${hours}deg)`;
 }
+setInterval(setDate,1000);
