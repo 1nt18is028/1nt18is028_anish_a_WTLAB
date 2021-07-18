c3f5d37819c2842db82b1aee091c21d417fdd9ff
1nt18is028 <83984563+1nt18is028@users.noreply.github.com>
2021-07-18 21:32:54 +0530

    First

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const panel=document.querySelectorAll('.panel');
-console.log(panel);
-console.log(1);
-panel.forEach(item=>item.addEventListener('click',()=>{
-    item.classList.toggle('open-active')
-}));
+var a = Math.floor(Math.random())* Math.floor(101);
+
+function check(){
+    var num=document.getElementById("num").value;
+    if(num==a){
+        alert("The number is correct");
+    }else if(num+10 < a){
+        alert("")
+    }else if
+}
